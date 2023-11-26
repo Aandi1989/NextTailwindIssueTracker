3467d271d761366b2fcb00a5cb680232c9219a8a
Aandi1989 <navaliny1989@gmail.com>
2023-11-26 13:09:33 +0300

    useRouter() --ProgrammaticNavigation

--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,8 +1,12 @@
+'use client';
+import { useRouter } from 'next/navigation';
 import React from 'react'
 
 const NewUserPage = () => {
+  const router = useRouter();
+
   return (
-    <div>NewUserPage</div>
+    <button onClick={() => router.push('/users')} className='btn btn-primary'>Create</button>
   )
 }
 
