d2789b0d5249e2e3fe4426122ff178f9a9e5b273
Aandi1989 <navaliny1989@gmail.com>
2023-11-26 20:57:36 +0300

    validation with Zod

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import schema from "./schema";
 
 export function GET(request: NextRequest) {
   return NextResponse.json([
@@ -11,8 +12,9 @@ export async function POST(request: NextRequest) {
   const body = await request.json();
   // Validate
   // If invalid, return 400
-  if (!body.name)
-    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  const validation = schema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
   // Else, return
   return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
 }
