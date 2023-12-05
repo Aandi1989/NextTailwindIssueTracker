3c50e3a218388593b2907b60f00ae6be620f43d9
Aandi1989 <navaliny1989@gmail.com>
2023-12-05 15:45:35 +0300

    example of private routing

--- middleware.ts
+++ middleware.ts
@@ -11,3 +11,21 @@ export const config = {
     // ?: zero or one
     matcher: ['/dashboard/:id*']
 }
+
+
+
+/*
+пример private routing  https://www.youtube.com/watch?v=cj1trlsQ0is&list=WL&index=1&t=15s
+
+import {sessionStatus} from "@utils/session";   // переменная в которой мы храним значение авторизован пользователь или нет
+import {NextRequest, NextResponse} from "next/server";
+
+const protectedRoutes = ["/middlewareside"];  // url доступ к которому хотим ограничить
+
+export default function middleware(req: any) {
+    if(!sessionStatus && protectedRoutes.includes(req.nextUrl.pathname)){
+        const absoluteURL = new URL("/", req.nextUrl.origin);
+        return NextResponse.redirect(absoluteURL.toString());
+    }
+}
+*/ 
