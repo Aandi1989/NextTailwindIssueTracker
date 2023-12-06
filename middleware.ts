9e2799ed0233aa3acebc84d895fcdfe101ff34f2
Aandi1989 <navaliny1989@gmail.com>
2023-12-06 11:11:23 +0300

    using cookies

--- middleware.ts
+++ middleware.ts
@@ -29,3 +29,39 @@ export default function middleware(req: any) {
     }
 }
 */ 
+
+
+/*
+пример использования cookies https://www.youtube.com/watch?v=NUHAc1wjL3A&list=WL&index=2&t=800s
+
+import { NextResponse } from 'next/server'
+
+export function middleware(request) {
+    parsed nextUrl object
+    const { pathname, searchParams } = request.nextUrl
+    console.log({ pathname, sort: searchParams.get('sort') })
+    return NextResponse.next()
+
+    redirecting 
+    return NextResponse.rewrite(new URL('/team', request.url))
+
+    reading request cookies
+    const allCookies = request.cookies.getAll()
+
+    setting response cookies
+    const response = NextResponse.next()
+    response. cookie.set({
+        name: 'next',
+        value: 'fast',
+        path: '/'
+    })
+    return response
+
+    responding with json
+    return NextResponse.json({ message: 'Hello from middleware' })
+
+    export const config = {
+        matcher: '/api/test'
+    }
+}
+*/
